b3f38797a5512216528aea5b7210ab4189e88828
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-03-29 20:46:54 -0500

    add buttons for like and dislike that toggle and only allows one button active

--- socialfeed/src/App.js
+++ socialfeed/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// import './Components/NavBar';
+// import './Components/CreatePost';
+// import './Components/DisplayPosts';
+// import './Components/Post';
+import CustomButtons from './Components/CustomButtons';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CustomButtons /> 
+     <h1>Hello World!</h1>
     </div>
   );
 }
---
+++ socialfeed/src/Components/CustomButtons.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "../App";
+
+const CustomButtons = (props) => {
+  const [likeButtonState, setLikeButtonState] = useState("inactive");
+  const [dislikeButtonState, setDislikeButtonState] = useState("inactive");
+  const handleClick = (event) => {
+    switch (event.target.name) {
+      case "likeButton":
+        if (likeButtonState === "inactive") {
+          setLikeButtonState("active");
+          setDislikeButtonState("inactive");
+        } else if (likeButtonState === "active") {
+          setLikeButtonState("inactive");
+        }
+        break;
+      case "dislikeButton":
+        if (dislikeButtonState === "inactive") {
+          setDislikeButtonState("active");
+          setLikeButtonState("inactive");
+        } else if (dislikeButtonState === "active") {
+          setDislikeButtonState("inactive");
+        }
+        break;
+      default:
+        break;
+    }
+    console.log(event.target.name + likeButtonState);
+  };
+
+  return (
+    <div>
+      <button
+        className={likeButtonState}
+        onClick={handleClick}
+        name="likeButton"
+      >
+        Like
+      </button>
+
+      <button
+        className={dislikeButtonState}
+        onClick={handleClick}
+        name="dislikeButton"
+      >
+        Dislike
+      </button>
+    </div>
+  );
+};
+
+export default CustomButtons;
